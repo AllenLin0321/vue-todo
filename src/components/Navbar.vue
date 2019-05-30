aeac855200bc445123e0d3496ad7558a67bd3ea2
Allen <glux7381@gmail.com>
2019-05-30 14:51:47 +0800

    #11 - Adding Routes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,19 +13,34 @@
       </v-btn>
     </v-toolbar>
 
-    <v-navigation-drawer app class="indigo" v-model="drawer">
-        <p>test</p>
-    </v-navigation-drawer>
+    <v-navigation-drawer app class="deep-purple lighten-2" v-model="drawer">
+      <v-list>
 
+        <v-list-tile v-for="(link, index) in links" :key="index" :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+      </v-list>
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            drawer: false
-        }
-    }
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: "dashboard", text: "Dashboard", route: "/" },
+        { icon: "folder", text: "My Projects", route: "/projects" },
+        { icon: "person", text: "Team", route: "/team" }
+      ]
+    };
+  }
 };
 </script>
--- src/router.js
+++ src/router.js
@@ -1,23 +1,31 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Dashboard from './views/Dashboard.vue';
+import Projects from './views/Projects.vue';
+import Team from './views/Team.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home,
+      path: '*',
+      redirect: '/'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/',
+      name: 'dashboard',
+      component: Dashboard,
     },
+    {
+      path: '/team',
+      name: 'team',
+      component: Team,
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: Projects,
+    }
   ],
 });
