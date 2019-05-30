1f7e1ac507eaae1b50315490eb1007c6daf9977d
Allen <glux7381@gmail.com>
2019-05-30 16:08:35 +0800

    #14 - Grid System

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="dashboard">
-    <h1>Dashboard</h1>
+    <h1 class="subheading grey--text">Dashboard</h1>
+    <v-container class="my-5">
+      <v-card flat class="pa-3">
+        <v-layout wrap>
+          <v-flex xs12 md6>
+            <!-- .caption - Smaller size text -->
+            <div class="caption grey--text">Project Title</div>
+            <div>Create a new website</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>The Allen</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due by</div>
+            <div>1st Jan 2020</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Status</div>
+            <div>ongoing</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
