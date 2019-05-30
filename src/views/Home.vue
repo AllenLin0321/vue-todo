7a68e8ac1c5e1570420f79ad1732cb176f54227e
Allen <glux7381@gmail.com>
2019-05-30 11:02:27 +0800

    Vuetify #5 Button - Icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,41 @@
 <template>
   <div class="home">
     <h1>HomePage</h1>
+
+    <!-- Button -->
+    <!-- <v-btn color="success">Click me</v-btn>
+    <v-btn dark depressed color="pink">Click me</v-btn>
+    <v-btn outline color="info">click me</v-btn>
+    <v-btn flat color="info">click me</v-btn>
+
+    <v-btn class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>Email me!</span>
+    </v-btn>
+
+    <v-btn small class="pink white--text">
+      <v-icon small left>email</v-icon>
+      <span>Email me!</span>
+    </v-btn>
+
+    <v-btn large class="pink white--text">
+      <v-icon large left>email</v-icon>
+      <span>Email me!</span>
+    </v-btn>
+
+    <v-btn large dark color="purple">
+      <v-icon large left>email</v-icon>
+      <span>Email me!</span>
+    </v-btn>
+
+    <v-btn fab small outline dark color="orange light-2">
+      <v-icon left>add_shopping_cart</v-icon>
+      <span>Email me!</span>
+    </v-btn> -->
+
   </div>
 </template>
 
 <script>
-
-  export default {
-  }
+export default {};
 </script>
